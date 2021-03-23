662dbdc2ffdc34aa7f791baea70e110fde270830
Vimarsh-Koul <vimarsh2001koul@gmail.com>
2021-03-23 19:53:20 +0530

    Mar 23

---
+++ Valid_Sudoku.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>> row(9, vector<int> (9,0));
+        vector<vector<int>> col(9, vector<int> (9,0));
+        vector<vector<int>> box(9, vector<int> (9,0));
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                    int k = (i/3)*3 + (j/3);
+                    int num = board[i][j]-'0';
+                    if(row[i][num-1] || col[j][num-1] || box[k][num-1]){
+                        return false;
+                    }
+                    
+                    row[i][num-1]=1;
+                    col[j][num-1]=1;
+                    box[k][num-1]=1;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
