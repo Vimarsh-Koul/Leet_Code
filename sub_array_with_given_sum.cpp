c429097118bce99f433b9530023ab80947eb4978
Vimarsh-Koul <vimarsh2001koul@gmail.com>
2021-04-01 22:07:42 +0530

    Apr 1

---
+++ sub_array_with_given_sum.cpp
@@ -0,0 +1,34 @@
+class Solution{
+    public:
+    // Function to find the subarray with given sum k
+    // arr: input array
+    // n: size of array
+    vector<int> subarraySum(int arr[], int n, int s){
+        
+      int i=0, j =0;
+      int sum =arr[0];
+      vector<int> ans;
+      while(i< n && j<n){
+          if(sum == s){
+              ans.push_back(i+1);
+              ans.push_back(j+1);
+              return ans;
+          }
+          
+          if(sum< s){
+              j++;
+              sum+=arr[j];
+          }
+          
+          else if(sum> s){
+              sum-= arr[i];
+             i++; 
+          }
+
+      }
+      
+    ans.push_back(-1);
+    return ans;
+        
+    }
+};
