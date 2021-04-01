1bb2d83e481a7fe51b4fb0f1160e5ea75097675c
Vimarsh-Koul <vimarsh2001koul@gmail.com>
2021-04-02 01:07:11 +0530

    Apr 2

--- sub_array_with_given_sum.cpp
+++ sub_array_with_given_sum.cpp
@@ -11,7 +11,7 @@ class Solution{
       while(i< n && j<n){
           if(sum == s){
               ans.push_back(i+1);
-              ans.push_back(j+1);
+              ans.push_back(j+1);s
               return ans;
           }
           
