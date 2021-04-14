97cee80a4fb534d3d021381f9862ef92f7ceadbb
Vimarsh-Koul <vimarsh2001koul@gmail.com>
2021-04-14 11:21:48 +0530

    Apr 14

---
+++ split_a_string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count =0;
+        int l =0, r=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='L')
+                l++;
+            else if(s[i]=='R')
+                r++;
+            
+            if(l-r == 0){
+                count++;
+                l=r=0;
+            }
+        }
+        
+        return count;
+    }
+};
